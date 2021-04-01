8d250258359b3cf561803772cd2dde8119ef8368
Marc Doms <marc.doms@adbsafegate.com>
2021-04-01 18:01:59 +0200

    wip

--- helloworld.sh
+++ helloworld.sh
@@ -2,9 +2,9 @@
 
 echo "# Checking Variables"
 if [ -z "$1" ]; then
-    echo "Hello World: no parameter <$1>"
+    echo "Hello World: no parameter <$1, $2>"
 else
-    echo "Hello World: $1"
+    echo "Hello World: $1, $2"
 fi
 
 pwd
