5d57106883f8a2bc16dc9df8b618d0ce59d14bb2
Marc Doms <marc.doms@adbsafegate.com>
2021-04-01 15:44:57 +0200

    wip

--- helloworld.sh
+++ helloworld.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+echo "begin"
 echo "Hello World"
 
 echo "Hello World" > "out1"
-echo "Hello World" > "./out2"
-echo "Hello World" > "~/out3"
+# echo "Hello World" > "./out2"
+# echo "Hello World" > "~/out3"
+
+echo "end"
